104340ac70ba73d9d2e5d49443fe56bbcbeb3b21
keshav-chaure <keshav.chaure@gmail.com>
2017-07-22 20:12:06 +0530

    pushing changes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Hi");
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var dt = require('./myfirstmodule');
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.write("The date and time are currently: " + dt.myDateTime());
+    //res.write(req.url);
+    //res.end('Hello World!');
+/*
+//http://localhost:8080/?year=2017&month=July
+     var q = url.parse(req.url, true).query;
+      var txt = q.year + " " + q.month;
+      res.end(txt);
+  */  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
